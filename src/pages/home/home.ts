5be4ff182bf14d4243624f73ee439b3bfe8d7211
John Olawale <mojoblanco@gmail.com>
2017-11-09 22:26:19 +0100

    handle error

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { NumberProvider } from '../../providers/number/number';
 
 @Component({
@@ -12,7 +12,7 @@ export class HomePage {
   type = 'trivia';
   result: any;
 
-  constructor(public navCtrl: NavController, private numberProvider: NumberProvider, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, private numberProvider: NumberProvider, public loadingCtrl: LoadingController, public alertCtrl: AlertController) {
     this.number = Math.floor((Math.random() * 100) + 1);
     this.getData();
   }
@@ -23,10 +23,22 @@ export class HomePage {
     });
     loader.present();
 
-    this.numberProvider.getNumberData(this.number, this.type).subscribe(data => {
-      loader.dismiss();
-      this.result = data.text();
-    });
+    this.numberProvider.getNumberData(this.number, this.type)
+      .subscribe(
+        data => {
+          loader.dismiss();
+          this.result = data.text();
+        },
+        error => {
+          loader.dismiss();
+          let alert = this.alertCtrl.create({
+            title: 'Error!',
+            subTitle: 'Unable to contact data source. Please try again!',
+            buttons: ['OK']
+          });
+          alert.present();    
+        }
+      );
     
   }
 
